60bc3f1fc9c040b90e994b19f077fff0b89c8055
izzat <izzatfadzlon@gmail.com>
2024-06-04 07:20:00 +0800

    bcrypt + hash function done)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,14 @@ const loginUser = async (req, res) => {
 
 // signup user
 const signupUser = async (req, res) => {
-  res.json({ message: "signup user" });
+  const { email, password } = req.body;
+  try {
+    //using signup method in userSchema
+    const user = await User.signup(email, password);
+    res.status(200).json({ email, user });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 module.exports = { loginUser, signupUser };
