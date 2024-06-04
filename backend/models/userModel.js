4c08e32307f0477f5e9942f9b24b214ecb108538
izzat <izzatfadzlon@gmail.com>
2024-06-05 00:48:47 +0800

    add comments

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -17,16 +17,18 @@ const userSchema = new Schema({
 
 //static signup method (similar to class methods in cpp/Java)
 userSchema.statics.signup = async function (email, password) {
-  //can't use "User" since not created in userModel file
+  //use 'this', can't use "User" since not created in userModel file
   const exists = await this.findOne({ email });
 
+  //if User email exists
   if (exists) {
     throw Error("Email already in use");
   }
-  //addition of "salt" to password, increases security
+  //increase security: addition of "salt" to password, then hash it
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
 
+  //this creates a new User in the Users db, tgt with hashed password
   const user = await this.create({ email, password: hash });
 
   return user;
