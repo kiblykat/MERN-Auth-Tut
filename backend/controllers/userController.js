1f8a243a5ff6e41f40e2246bdd7f6aed7cf2540f
kiblykat <izzatfadzlon@gmail.com>
2024-06-05 18:21:55 +0800

    added jwt authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,10 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+//create a function that returns token (with expiry and SECRET key)
+const createToken = (_id) => {
+  return jwt.sign({ _id: _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 // login user
 const loginUser = async (req, res) => {
@@ -11,7 +17,12 @@ const signupUser = async (req, res) => {
   try {
     //using signup method in userSchema to create the User
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+
+    //create a token
+    const token = createToken(user._id);
+
+    //now, we are passing the token to the browser as the second property of the response object
+    res.status(200).json({ email, token });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
